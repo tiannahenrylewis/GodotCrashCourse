b53223d0d06a23e0740cf1489d31a2834b73939b
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2023-03-18 15:12:55 +0100

    S2: Victory and Defeat Screens

--- Scenes/Main/main.gd
+++ Scenes/Main/main.gd
@@ -1,19 +1,13 @@
-extends Node
+extends Node2D
 
 @export var end_screen_scene: PackedScene
 
-@onready var timer = $Timer
-
 
 func _ready():
-	timer.timeout.connect(on_timer_timeout)
-
-
-func get_time_elapsed():
-	return timer.wait_time - timer.time_left
-
-
-func on_timer_timeout():
+	$%Player.health_component.died.connect(on_player_died)
+	
+	
+func on_player_died():
 	var end_screen_instance = end_screen_scene.instantiate()
 	add_child(end_screen_instance)
 	end_screen_instance.set_defeat()
