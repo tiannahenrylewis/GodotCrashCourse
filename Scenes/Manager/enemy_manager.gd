df2304c61457c394f78e4369c13fc3a16c588b0c
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2023-03-20 22:31:51 +0100

    S3: TileMap Improvements

--- Scenes/Manager/enemy_manager.gd
+++ Scenes/Manager/enemy_manager.gd
@@ -40,5 +40,4 @@ func on_timer_timeout():
 func on_arena_difficulty_increased(arena_difficulty: int):
 	var time_off = (0.1 / 12) * arena_difficulty
 	time_off = min(time_off, 0.7)
-	print(time_off)
 	timer.wait_time = base_spawn_time - time_off
