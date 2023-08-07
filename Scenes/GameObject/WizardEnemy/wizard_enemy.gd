189bdac559941f225d0e802a9f9819e9647e4d38
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2023-08-07 16:02:04 +0200

    S3: Add Wizard Enemy

--- Scenes/GameObject/WizardEnemy/wizard_enemy.gd
+++ Scenes/GameObject/WizardEnemy/wizard_enemy.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
 
 @onready var velocity_component = $VelocityComponent
+@onready var visuals = $Visuals
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	velocity_component.accelerate_to_player()
 	velocity_component.move(self)
+	
+	var move_sign = sign(velocity.x)
+	if move_sign != 0:
+		visuals.scale = Vector2(move_sign, 1)
