8946648f63011c9f24885587c5819385f0c0a56f
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2023-10-14 01:45:47 -0400

    S4: SFX - Combat

--- Scenes/GameObject/BasicEnemy/basic_enemy.gd
+++ Scenes/GameObject/BasicEnemy/basic_enemy.gd
@@ -2,7 +2,14 @@ extends CharacterBody2D
 
 @onready var velocity_component = $VelocityComponent
 
+func _ready():
+	$HurtboxComponent.hit.connect(on_hit)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	velocity_component.accelerate_to_player()
 	velocity_component.move(self)
+
+
+func on_hit():
+	$HitRandomAudioPlayerComponent.play_random()
--- Scenes/GameObject/ExperienceVial/experience_vial.gd
+++ Scenes/GameObject/ExperienceVial/experience_vial.gd
@@ -45,4 +45,4 @@ func on_area_entered(other_area: Area2D):
 	
 	tween.tween_callback(collect)
 	
-	
+	$RandomStreamPlayer2DComponent.play_random()
