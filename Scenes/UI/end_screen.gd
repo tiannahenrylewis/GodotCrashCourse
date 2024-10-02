5e69e0369a8ffbcbb14947069f5b68d0a2ced6ee
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2024-10-01 22:45:52 -0400

    S5: Scene Transition

--- Scenes/UI/end_screen.gd
+++ Scenes/UI/end_screen.gd
@@ -35,6 +35,8 @@ func set_victory():
 	
 
 func on_restart_button_pressed():
+	ScreenTransition.transition()
+	await ScreenTransition.transitioned_halfway
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/Main/main.tscn")
 	
